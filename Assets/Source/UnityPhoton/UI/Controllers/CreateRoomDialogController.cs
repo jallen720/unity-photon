be2b64d330ef236bc37628302b905d16be8eeeba
Joel Allen <joelallen720@gmail.com>
2016-05-31 11:18:08 -0700

    Show error messages when an invalid room name is entered int he create room dialog.

--- Assets/Source/UnityPhoton/UI/Controllers/CreateRoomDialogController.cs
+++ Assets/Source/UnityPhoton/UI/Controllers/CreateRoomDialogController.cs
@@ -1,4 +1,7 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 using UnityEngine.UI;
 
 namespace UnityPhoton {
@@ -11,20 +14,73 @@ namespace UnityPhoton {
         private Button createButton;
 
         [SerializeField]
+        private Button cancelButton;
+
+        [SerializeField]
         private Text roomNameText;
+
+        [SerializeField]
+        private ErrorDisplay errorDisplay;
+
+        private readonly List<Validation<string>> roomNameValidations =
+            new List<Validation<string>> {
+                new Validation<string> {
+                    isValid = (string roomName) => roomName != "",
+                    errorMessage = "Room name cannot be empty"
+                },
+                new Validation<string> {
+                    isValid = IsUnique,
+                    errorMessage = "A room with that name already exists"
+                }
+            };
 
         private void Start() {
             createButton.onClick.AddListener(CheckCreateRoom);
+            cancelButton.onClick.AddListener(errorDisplay.Clear);
         }
 
         private void CheckCreateRoom() {
-            CreateRoom();
+            List<string> errorMessages;
+
+            if (RoomNameIsValid(out errorMessages)) {
+                CreateRoom();
+            }
+            else {
+                errorDisplay.SetErrors(errorMessages);
+            }
+        }
+
+        private bool RoomNameIsValid(out List<string> errorMessages) {
+            errorMessages = new List<string>(
+                roomNameValidations
+                    .FindAll(validation => !validation.isValid(roomNameText.text))
+                    .Select(validation => validation.errorMessage));
+
+            return errorMessages.Count == 0;
         }
 
         private void CreateRoom() {
             lobbyController.FlagAsJoiningRoom();
             PhotonNetwork.CreateRoom(roomNameText.text);
+            Close();
+        }
+
+        private void Close() {
+            errorDisplay.Clear();
             gameObject.SetActive(false);
+        }
+
+        private struct Validation<T> {
+            public Predicate<T> isValid;
+            public string errorMessage;
+        }
+
+        // Static members
+
+        private static bool IsUnique(string roomName) {
+            return !Array.Exists(
+                PhotonNetwork.GetRoomList(),
+                (RoomInfo roomInfo) => roomInfo.name == roomName);
         }
     }
 }
---
+++ Assets/Source/UnityPhoton/UI/Displays/ErrorDisplay.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityUtils.Extensions;
+using UObject = UnityEngine.Object;
+
+namespace UnityPhoton {
+
+    [RequireComponent(typeof(RectTransform))]
+    [RequireComponent(typeof(Image))]
+    public class ErrorDisplay : MonoBehaviour {
+
+        [SerializeField]
+        private UObject errorMessagePrefab;
+
+        private RectTransform rectTransform;
+        private Image background;
+        private List<string> currentErrors;
+
+        private void Start() {
+            rectTransform = GetComponent<RectTransform>();
+            background = GetComponent<Image>();
+            currentErrors = new List<string>();
+            SetVisibility();
+        }
+
+        public void SetErrors(IEnumerable<string> errors) {
+            currentErrors.Clear();
+            currentErrors.AddRange(errors);
+            UpdateErrorMessages();
+        }
+
+        private void UpdateErrorMessages() {
+            DestroyOldErrorMessages();
+            currentErrors.ForEach(LoadErrorMessage);
+            SetVisibility();
+        }
+
+        private void DestroyOldErrorMessages() {
+            foreach (Transform errorMessage in rectTransform) {
+                Destroy(errorMessage.gameObject);
+            }
+        }
+
+        private void LoadErrorMessage(string error) {
+            rectTransform.InstantiateChild<Text>(errorMessagePrefab).text = error;
+        }
+
+        private void SetVisibility() {
+            background.enabled = currentErrors.Count > 0;
+        }
+
+        public void Clear() {
+            currentErrors.Clear();
+            UpdateErrorMessages();
+        }
+    }
+}
